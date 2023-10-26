bfdd32c395f5b470ca3daf23c6fbe2547b22a327
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2023-10-26 21:55:03 +0300

    feat: split vendors

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vitest/config"
 import react from "@vitejs/plugin-react"
 import tsconfigPaths from "vite-tsconfig-paths"
+import { splitVendorChunkPlugin } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
   server: {
     open: true,
   },
